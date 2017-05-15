0c39b05fada8893bee1fbce2be71d4106cb0e6cb
ngurajeka <ngurajeka.92@gmail.com>
2017-05-15 14:34:21 +0800

    extract result into upperdb

---
+++ extractor/upperdb.go
@@ -0,0 +1,16 @@
+package extractor
+
+import (
+	"fmt"
+
+	querybuilder "github.com/ngurajeka/go-querybuilder"
+	db "upper.io/db.v3"
+)
+
+func AsUpperDBCondition(q *querybuilder.QueryBuilder) (conds []interface{}) {
+	for _, cond := range (*q).Conditions() {
+		field := fmt.Sprintf("%s %s", cond.Field(), cond.Operator())
+		conds = append(conds, db.Cond{field: cond.Value()})
+	}
+	return
+}
